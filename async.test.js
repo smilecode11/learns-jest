08024a255cebad24b70c315cd69cc2f4c41bee13
尘心 <smilewupeng@gmail.com>
2021-12-30 17:00:35 +0800

    feat:async test

---
+++ async.test.js
@@ -0,0 +1,48 @@
+/** callback */
+function fetchData(callback) {
+    setTimeout(() => {
+        callback('settimeout')
+    }, 700)
+}
+
+test('测试 fetchData 返回值', (done) => {
+    fetchData((data) => {
+        expect(data).toBe('settimeout')
+        done()
+    })
+})
+
+/** promise */
+function fetchDataWithPromise() {
+    return Promise.resolve('promise async')
+}
+test('测试 promise fetchData', () => {
+    return fetchDataWithPromise().then(data => {
+        expect(data).toBe('promise async')
+    })
+})
+
+function fetchDataWithPromiseReject() {
+    return Promise.reject('promise async reject')
+}
+test('测试 promise fetchData reject', () => {
+    return fetchDataWithPromiseReject().catch(err => {
+        expect(err).toMatch('reject')
+    })
+})
+//  .resolves | .rejects
+test('测试 .resolves | .rejects', () => {
+    expect(fetchDataWithPromise()).resolves.toBe('promise async')
+    expect(fetchDataWithPromiseReject()).rejects.toBe('promise async reject')
+})
+
+/** async */
+function fetchDataWithAsync() {
+    return Promise.resolve('async function')
+}
+test('测试 async function', async () => {
+    const result = await fetchDataWithAsync()
+    expect(result).toBe('async function')
+    await expect(fetchDataWithAsync()).resolves.toMatch(/async/)
+})
+
